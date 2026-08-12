e76ff7534a6292f23c4a9aa3fc6e2bc210e390b3
Shivam2-tech <shindeshivampatil2008@gmail.com>
2026-08-12 20:19:11 +0530

    Did some debugging

--- 06-MERN Stack Projects/notes-app/backend/config/db.js
+++ 06-MERN Stack Projects/notes-app/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function DBConnect() {
     try {
-        const url = process.env.MONGODB_URL || "mongodb://localhost:27017/notes-app";
+        const url = process.env.MONGO_URL || "mongodb://localhost:27017/notes-app";
         await mongoose.connect(url);
         console.log("MongoDB Connected Successfully");
     } catch (error) {
